3cb6cdb272b208536bcfe0c0732dbe7501ae50ba
vimal patel <pvimal816@gmail.com>
2021-01-30 08:22:35 +0530

    take measurements on iisc server

--- measure.sh
+++ measure.sh
@@ -3,12 +3,12 @@
 #SBATCH --ntasks=1 # Run on a eight CPU
 #SBATCH --time=00:050:00 # Time limit hrs:min:sec
 #SBATCH --output=measure%j.out # Standard output and error log
-#SBATCH --partition=q1m_2h-1G
+#SBATCH --partition=q2h_24h-2G
 
 hostname;
 
-cd ~/hpca-assignment-2020-2021/PartB;
-nvcc -o diag_mult_server ./main.cu -I ./header/;
+cd ~/hpca-prog-assignment/PartB;
+nvcc -o diag_mult_server ./main.cu -I ./header/ -std=c++11;
 
 echo "==========================================================================";
 nvprof ./diag_mult_server ./data/input_4096.in;
@@ -18,4 +18,6 @@ echo "==========================================================================
 nvprof ./diag_mult_server ./data/input_16384.in;
 echo "==========================================================================";
 nvprof ./diag_mult_server ./data/input_32768.in;
+echo "==========================================================================";
+nvprof ./diag_mult_server ./data/input_65536.in;
 echo "==========================================================================";
